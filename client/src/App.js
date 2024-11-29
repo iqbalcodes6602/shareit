448dcfeb8f4db1ce502ae6322ed2e1781745ac9b
Anas Iqbal <mohd.abd.6602@gmail.com>
2024-11-29 22:02:19 +0530

    Update App styles and remove unused image reference

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,6 @@ function App() {
 
   const fileInputRef = useRef();
 
-  const url = 'https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg';
-
   useEffect(() => {
     const getImage = async () => {
       if (file) {
@@ -30,7 +28,6 @@ function App() {
 
   return (
     <div className='container'>
-      <img src={url} className='img' />
       <div className='wrapper'>
         <h1>Simple file sharing!</h1>
         <p>Upload and share the download link.</p>
